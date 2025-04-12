145d66dc826c5818de6a47873b490411d9d5d0aa
nayan1306 <vnayan1306@gmail.com>
2025-04-12 23:00:15 +0530

    added name and taglien

--- lib/screens/section_one.dart
+++ lib/screens/section_one.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio_website/widgets/git_hub_button.dart';
 import 'package:portfolio_website/widgets/hover_button.dart';
 
@@ -15,6 +16,8 @@ class SectionOne extends StatelessWidget {
       height: responsiveHeight,
       decoration: const BoxDecoration(color: Color.fromARGB(0, 0, 0, 0)),
       child: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Responsive Custom AppBar
           Container(
@@ -22,21 +25,19 @@ class SectionOne extends StatelessWidget {
               horizontal: screenWidth * 0.11,
               vertical: 12,
             ),
-
             child: Row(
               children: [
                 Image.asset("assets/logo_nv.png", width: 30, height: 30),
                 const SizedBox(width: 2),
                 Expanded(
                   child: Text(
-                    "V",
+                    "",
                     style: Theme.of(context).textTheme.titleLarge?.copyWith(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-
                 GitHubButton(),
                 HoverButton(buttonText: "Sponser", onPressed: () {}),
                 SizedBox(width: 20),
@@ -47,13 +48,54 @@ class SectionOne extends StatelessWidget {
 
           // Section Body
           Expanded(
-            child: Center(
-              child: Text(
-                "Welcome to Section One",
-                style: Theme.of(
-                  context,
-                ).textTheme.headlineSmall?.copyWith(color: Colors.white),
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: screenWidth * 0.11,
+                    top: screenHeight * 0.1,
+                  ),
+                  child: SizedBox(
+                    width: screenWidth * 0.35,
+                    // padding: const EdgeInsets.symmetric(
+                    //   horizontal: 40,
+                    //   vertical: 20,
+                    // ),
+                    // color: Colors.amber,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 20),
+                        Text(
+                          "Nayan Verma",
+                          style: Theme.of(
+                            context,
+                          ).textTheme.headlineMedium?.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 90,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: screenWidth * 0.28,
+                          child: Text(
+                            "Crafting pixel-perfect Flutter apps with precision and creativity. I'm driven by innovation, constantly exploring new technologies to build seamless and intuitive experiences.",
+                            style: GoogleFonts.orbitron(
+                              color: const Color.fromARGB(197, 255, 255, 255),
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                              letterSpacing: 1.8,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
