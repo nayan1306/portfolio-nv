bb478bbb4a114cd09dce99c4f4af618eb009e614
nayan1306 <vnayan1306@gmail.com>
2025-04-14 18:48:39 +0530

    section 4 busy

--- lib/screens/s4/section_four.dart
+++ lib/screens/s4/section_four.dart
@@ -9,10 +9,11 @@ class SectionFour extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
+    final bool isMobile = screenWidth < 600;
 
     return Container(
       width: double.infinity,
-      height: screenHeight * 1.58,
+      height: isMobile ? screenHeight * 1.23 : screenHeight * 1.58,
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
@@ -37,18 +38,18 @@ class SectionFour extends StatelessWidget {
                 textStyle: Theme.of(context).textTheme.headlineMedium?.copyWith(
                   color: Colors.black,
                   fontWeight: FontWeight.bold,
-                  fontSize: 90,
+                  fontSize: isMobile ? 40 : 90,
                   letterSpacing: 2,
                 ),
               ),
             ),
             const SizedBox(height: 20),
             Text(
-              "Stacked with certs. Fueled by curiosity. Here’s proof I’m built for the future.",
+              "Stacked with certs. Fueled by curiosity.",
               style: Theme.of(context).textTheme.titleLarge?.copyWith(
                 color: Colors.black87,
                 fontWeight: FontWeight.w400,
-                fontSize: 20,
+                fontSize: isMobile ? 14 : 20,
                 letterSpacing: 1.2,
               ),
             ),
@@ -56,13 +57,13 @@ class SectionFour extends StatelessWidget {
             SizedBox(height: screenHeight * 0.05),
 
             GridView.count(
-              crossAxisCount: 3,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
+              crossAxisCount: isMobile ? 2 : 3,
+              crossAxisSpacing: isMobile ? 12 : 20,
+              mainAxisSpacing: isMobile ? 12 : 20,
               padding: const EdgeInsets.all(16),
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
-              childAspectRatio: 1.5, // increased ratio for smaller cards
+              childAspectRatio: isMobile ? 1.2 : 1.5, // Larger for mobile
               children: [
                 GlassCertificateCard(
                   imageUrl:
