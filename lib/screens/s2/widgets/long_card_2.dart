605cdf7090d59b00a147964b2d65126bddf37aa8
nayan1306 <vnayan1306@gmail.com>
2025-04-14 21:49:05 +0530

    modified sensors

--- lib/screens/s2/widgets/long_card_2.dart
+++ lib/screens/s2/widgets/long_card_2.dart
@@ -64,23 +64,7 @@ class LongCard2 extends StatelessWidget {
     return Center(
       child: Tilt(
         borderRadius: BorderRadius.circular(24),
-        tiltConfig: TiltConfig(
-          angle: 15.0,
-          enableGestureSensors:
-              !kIsWeb && (Platform.isAndroid || Platform.isIOS),
-          enableReverse: true,
-          filterQuality: FilterQuality.medium,
-          enableGestureHover: true,
-          enableGestureTouch: true,
-          enableRevert: true,
-          moveDuration: const Duration(milliseconds: 120),
-          leaveDuration: const Duration(milliseconds: 350),
-          moveCurve: Curves.easeOutCubic,
-          leaveCurve: Curves.easeOut,
-          sensorFactor: 8.0,
-          enableSensorRevert: true,
-          sensorRevertFactor: 0.08,
-        ),
+        tiltConfig: const TiltConfig(angle: 40, enableRevert: true),
         child: Container(
           width: width,
           height: height,
